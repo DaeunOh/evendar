35c7d5491e170c43c614a088a56c5f8e541ee51f
DaeunOh <dev.carrotoh@gmail.com>
2024-10-25 17:51:47 +0900

    chore: add prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true, // Use single quotes
+  semi: true, // Use semicolons at the end of statements
+  useTabs: false, // Disallow the use of tabs and replace with spaces
+  tabWidth: 2, // Tab size
+  trailingComma: 'all', // Always add a comma after the last key:value pair in objects and arrays
+  printWidth: 120, // Maximum length per line
+  arrowParens: 'avoid', // Omit parentheses when a single parameter is used in an arrow function
+};
