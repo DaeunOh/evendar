fb848152a3883fa181d8680270c74b8e35d9a89c
DaeunOh <dev.carrotoh@gmail.com>
2024-10-29 16:57:21 +0900

    feat: add color theme

--- src/constants/theme/theme.d.ts
+++ src/constants/theme/theme.d.ts
@@ -1,7 +1,33 @@
 import '@emotion/react';
 
+interface ColorScale {
+  50: string;
+  100: string;
+  200: string;
+  300: string;
+  400: string;
+  500: string;
+  600: string;
+  700: string;
+}
+
+interface ColorOpacity {
+  100: string;
+  70: string;
+  50: string;
+  40: string;
+  20: string;
+}
+
 declare module '@emotion/react' {
   export interface Theme {
+    color: {
+      system: { primary: string };
+      white: ColorOpacity & { 0: string };
+      gray: ColorScale & { 800: string; 900: string };
+      black: ColorOpacity & { 80: string; 30: string; 10: string; 6: string; 4: string };
+      validation: { negative: string };
+    };
     text: {
       xxxxl: {
         bold: string;
